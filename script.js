f97ee63005c60739c534fa526fc6458cf0c482c0
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2025-12-02 09:33:23 -0500

    Write reset function

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ PSEUDOCODE
 - Adds or subtracts 1 number of habits completed (depending on true or false for checkmark).
 - Updates that amount of habits that were completed.
 - User resets button at the end of the day or start of the new one.
+    -- reset button will call 2 functions, 1 that will reset the checkboxes and the other that will reset the number of habits completed
+*/
 
- */
+// function to reset the checklist
+function resetChecklist() {
+    document.getElementById('habit-checklist').reset();
+}
