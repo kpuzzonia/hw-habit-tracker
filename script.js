22251b502d71f9dff3d3af7700dea434afff6ca7
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2025-12-02 09:07:58 -0500

    Refine pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+PSEUDOCODE
+
+- User checks box.
+- Event listener notices.
+- Adds or subtracts 1 number of habits completed (depending on true or false for checkmark).
+- Updates that amount of habits that were completed.
+- User resets button at the end of the day or start of the new one.
+
+ */
